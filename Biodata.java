7dbcbf94f1bda93608df52bede9dbb10973d8b06
Exia567 <120551312+Exia567@users.noreply.github.com>
2025-03-04 21:07:18 +0700

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,31 @@
+package tugasjava; 
+
+
+public class Biodata{
+    public static void main(String[] args){
+        String nim;
+        nim = "A11.2023.14894";
+        String nama;
+        nama = "Mahardika Wahyu Eka Putra";
+        String alamat;
+        alamat = "Jl.Widosari Raya Gang VI";
+        String kota;
+        kota = "Semarang";
+        String kodepos;
+        kodepos = "5431";
+        String telepon;
+        telepon = "088455036";
+        String hp;
+        hp = "03456";
+        String email;
+        email = "dikadiko@gmail.com";
+        System.out.println("NIM : "+nim);
+        System.out.println("Nama : "+nama);
+        System.out.println("Alamat : "+alamat);
+        System.out.println("Kota : "+kota);
+        System.out.println("Kode Pos : "+kodepos);
+        System.out.println("Telp : "+telepon);
+        System.out.println("No Hp : "+hp);
+        System.out.println("Email : "+email);   
+    }
+}
